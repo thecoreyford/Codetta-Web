bc4636ea719588033aa76f2e84c9b62639c493bf
Corey Ford <corey2.ford@live.uwe.ac.uk>
2020-04-02 20:19:34 +0100

    MOD: Messed around adding piano icon to block

--- codetta-blocks/instrument_blocks.js
+++ codetta-blocks/instrument_blocks.js
@@ -10,44 +10,30 @@ goog.require('Blockly.Blocks');
 
 //========================================================================================
 
-/** Array of instruments, media files should match*/
-var inst = [
-	"piano", 
-	"glock", 
-	"violin", 
-	"viola", 
-	"cello", 
-	"trumpet", 
-	"synth"
-];
-
-for(var i = 0; i < inst.length; ++i){
-	Blockly.Blocks[inst[i]] = {
-	  /**
-	   * Instrument block
-	   */
-	  init: function() {
-	    this.jsonInit({
-	      "id": inst[i],
-	      "message0": "%1",
-	      "args0": [
-	        {
-	          "type": "field_image",
-	          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/event_whenflagclicked.svg", //TODO: replace with media
-	          "width": 40,
-	          "height": 40,
-	          "alt": "piano",
-	          "flip_rtl": true
-	        }
-	      ],
-	      "inputsInline": true,
-	      "nextStatement": null,
-	      "colour": "#663399",
-	      "colourSecondary": "#000000",
-	      "colourTertiary": "#000000"
-	    });
-	  }
-	};
-}
+/** Piano Block */
+Blockly.Blocks["piano"] = {
+  init: function() {
+    this.jsonInit({
+      "id": "piano",
+      "message0": "%1",
+      "args0": [
+        {
+          "type": "field_image",
+          "src": "https://raw.githubusercontent.com/thecoreyford/Codetta-Web/master/codetta-blocks/media/instruments/" + "piano" + ".png",
+          "width": 40,
+          "height": 40,
+          "alt": "piano",
+          "flip_rtl": false
+        }
+      ],
+      "inputsInline": true,
+      "nextStatement": null,
+      "colour": "#663399",
+      "colourSecondary": "#000000",
+      "colourTertiary": "#000000"
+    });
+  }
+};
 
 
+//========================================================================================
