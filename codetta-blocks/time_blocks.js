70c8a3e36dd3f337fb55c3ea92f108115ecc8464
Corey Ford <corey2.ford@live.uwe.ac.uk>
2020-04-05 16:39:13 +0100

    MOD: Shrunk repeater block code from JSON to javascript

--- codetta-blocks/time_blocks.js
+++ codetta-blocks/time_blocks.js
@@ -13,45 +13,17 @@ goog.require('Blockly.Blocks');
 /**
   * Block for repeating bars of music.
   */
-Blockly.Blocks['repeat'] = {
+Blockly.Blocks['repeater'] = {
   init: function() {
-    this.jsonInit({
-      "id": "repeat",
-      "message0": "%1 %2 %3 %4",
-      "args0": [
-        {
-          "type": "field_image",
-          "src": "https://www.gstatic.com/codesite/ph/images/star_on.gif",
-          "width": 15,
-          "height": 15,
-          "alt": "*",
-          "flip_rtl": false
-        },
-        {
-          "type": "input_statement",
-          "name": "SUBSTACK"
-        },
-        {
-          "type": "field_image",
-          "src": "https://www.gstatic.com/codesite/ph/images/star_on.gif",
-          "width": 15,
-          "height": 15,
-          "alt": "*",
-          "flip_rtl": false
-        },
-        {
-          "type": "input_value",
-          "name": "TIMES",
-          "check": "Number"
-        }
-      ],
-      "inputsInline": true,
-      "previousStatement": null,
-      "nextStatement": null,
-      "colour": "#ffff00",
-      "colourSecondary": "#000000",
-      "colourTertiary": "#000000"
-    });
+    this.appendStatementInput("repeater")
+      .setCheck("Number");
+    this.appendValueInput("TIMES")
+        .setCheck("Number");
+    this.setColour("#ffff00", "#000000", "#000000");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+
+
   }
 };
 
