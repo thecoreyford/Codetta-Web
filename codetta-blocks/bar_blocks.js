572403768a175d8549981b135bc022dbbd7c84e9
Corey Ford <corey2.ford@live.uwe.ac.uk>
2020-04-10 12:16:16 +0100

    ADD: Scaffolding for a bar block

---
+++ codetta-blocks/bar_blocks.js
@@ -0,0 +1,29 @@
+/**
+ * bar_blocks.js
+ * @fileoverview Bar Blocks for Codetta
+ * @author corey2.ford@live.uwe.ac.uk <Corey Ford>
+ * @date 09 April 2020
+ */
+'use strict';
+
+goog.require('Blockly.Blocks');
+
+/**
+  * Block to set the tempo
+  */
+Blockly.Blocks['fourfour_bar'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Codetta.ClickableImage("https://raw.githubusercontent.com/thecoreyford/Codetta-Web/master/codetta-blocks/media/tempo_setter.png",
+                                              80, /* width */
+                                              80, /* height */
+                                              "*", /* alt_text */
+                                              false, /* rtl? */
+                                              -4 , -8), /* x and y position */
+                                              'tempo_setter');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#008000", "#000000", "#000000");
+  }
+};
+
